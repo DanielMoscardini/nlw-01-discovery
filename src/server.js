411ecf40e108925d7fcef59fa484528be749a1ee
Daniel Moscardini <moscardinibdaniel@gmail.com>
2022-07-18 15:24:12 -0300

    Finalizando projeto NLW 1 Starter

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const server = express()
 
+// Pegar o banco de dados.
+const db = require('./database/db.js')
+
 // Configurar pasta pública.
 server.use(express.static('public'))
+
+// Habilitar o uso do req.body na nossa aplicação.
+server.use(express.urlencoded({ extended: true }))
 
 // Utilizando template engine.
 const nunjucks = require('nunjucks')
@@ -14,17 +20,81 @@ nunjucks.configure('src/views', {
 // Configurar caminhos da minha aplicação:
 // Página inicial.
 server.get('/', (req, res) => {
-    return res.render('index.html')
+    return res.render('index.html', { title: 'Um titulo' })
 })
+
+
+
+
 
 // Cadastro de ponto de coleta.
 server.get('/create-point', (req, res) => {
+
+
     return res.render('create-point.html')
 })
 
+
+
+
+server.post('/savepoint', (req, res) => {
+
+    const query = `
+    INSERT INTO places (
+        image, name, address, address2, state, city, items
+    ) VALUES (
+        ?, ?, ?, ?, ?, ?, ?
+    )
+`
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err);
+            return res.send('Erro no cadastro')
+        }
+
+        console.log('Cadastrado com sucesso');
+        console.log(this);
+        return res.render('create-point.html', { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
+
+
+
+
 // Pesquisa de ponto de coleta.
 server.get('/search', (req, res) => {
-    return res.render('search-results.html')
+
+    const search = req.query.search
+    if (search == '') {
+        return res.render('search-results.html', { total: 0 })
+    }
+
+    // Consultar os dados do banco de dados.
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err) {
+            return console.log(err);
+        }
+
+        // Qtde de items.
+        const total = rows.length
+
+        // Mostrar a página HTML com os dados do banco de dados.
+        return res.render('search-results.html', { places: rows, total: total })
+    })
 })
 
 // Ligar o servidor.
